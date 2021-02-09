cddaf79cea295606ab9554066caca135e80c7ffa
RomainM27 <70415526+RomainM27@users.noreply.github.com>
2021-02-09 13:47:16 +0100

    Update hangman.js

--- assets/js/hangman.js
+++ assets/js/hangman.js
@@ -58,7 +58,7 @@ allButton.forEach((button) =>{
 // at the end its the replace
 let buttonPop = document.querySelector('#pop-up button');
 buttonPop.addEventListener("click", () =>{
-    location.pathname = '/index.html'
+    location.href = 'https://romainm27.github.io/index.html'
 })
 
 // =============================== function ==============
